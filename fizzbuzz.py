2f37e513fb7008e5b79b91654661e7ae32d7ca8d
JrpHub1 <jrewlp@gmail.com>
2026-01-15 11:17:07 +0900

    feat: Update fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 # feat: Create fizzbuzz.py
 
 for i in range(16, 30+1):
-    if i % 3 == 0:
+    if i % 15 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
         print('fizz')
     elif i % 5 == 0:
         print('buzz')
